c270aefbbc345c0d2409d08d352a8a2e4e370a3d
Sanjukta-Das22 <63544579+Sanjukta-Das22@users.noreply.github.com>
2021-04-07 14:45:44 +0530

    Add files via upload

---
+++ CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BankingLib;
+using Banking.Models;
+namespace Banking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var rm = (RegisterModel)Session["User"];
+           
+            ViewBag.User = rm.Account_Name;
+            return View(rm);
+        }
+  
+        public ActionResult Show()
+        {
+            var rm = (RegisterModel)Session["User"];
+            AccountModel acdta = new AccountModel();
+            AccountDAL dal = new AccountDAL();
+            Account ac = new Account();
+           
+            ac = dal.ShowDetails(rm.Account_No);
+
+            acdta.Account_NO = ac.Account_NO;
+            acdta.Account_Balance = ac.Account_Balance;
+            return View(acdta);
+        }
+    }
+}
