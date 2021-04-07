2dffca90d60f3fffcb14c40e5afd8bb0194bef07
Sanjukta-Das22 <63544579+Sanjukta-Das22@users.noreply.github.com>
2021-04-07 14:46:14 +0530

    Add files via upload

---
+++ AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Banking.Models
+{
+    public class AccountModel
+    {
+        public long Account_NO { get; set; }
+        public long Account_Balance { get; set; }
+    }
+}
